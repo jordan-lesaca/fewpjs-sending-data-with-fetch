37170ddcf3a5b7467246dc9b665af10f1b600b18
Jordan Lesaca <jordan.m.lesaca@gmail.com>
2021-07-12 14:42:50 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-// Add your code here
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+//   };
+
+  function submitData(userName, userEmail){
+    let data = {
+        name: userName,
+        email: userEmail
+    }
+    
+    const configObj = {
+      method: "POST", 
+      headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+      },
+      body: JSON.stringify(data)
+  }
+  return fetch("http://localhost:3000/users", configObj)
+
+  .then(function(response){
+      return response.json();
+  })
+  .then(function(object){
+      let h2 = document.createElement('h2')
+      h2.innerHTML = object.id
+      document.body.appendChild(h2)
+  })
+
+  .catch(function(error){
+      let h3 = document.createElement('h3')
+      h3.innerHTML = error.message
+      document.body.appendChild(h3)
+      alert("SOMETHING IS WRONG!")
+})
+
+}  
+//   const configurationObject = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData)
+//   };
+  
+//   fetch("http://localhost:3000/dogs", configurationObject); 
